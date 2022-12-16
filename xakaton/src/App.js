c96409df90a1a3e7db4836b9ef22ecaf90ea6f3a
DanilRev04 <danilrev04@gmail.com>
2022-12-17 00:29:36 +0300

    one

---
+++ xakaton/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from "./components/Ex";
+import AlertDismissibleExample from "./components/Ex";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AlertDismissibleExample />
+    </div>
+  );
+}
+
+export default App;
